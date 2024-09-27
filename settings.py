6bfb5214c5352319e7957145294d0625e54c7d1e
M0nyy <YegorMonahow@yandex.ru>
2024-09-27 09:35:46 +0000

    add querie for test in settings

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ victoria_url = "http://127.0.0.1:8428"
 victoria_url_to_send = "http://127.0.0.1:8428/api/v1/import/prometheus"
 
 queries = {
-  
+  "cpu_usage_percent": "cpu_usage_percent",  # test
 }
 
 filename = "metrics.csv"
